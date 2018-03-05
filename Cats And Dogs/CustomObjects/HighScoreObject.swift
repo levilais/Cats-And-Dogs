555983bd6c1d3572ec196306cd4c8a97536c5968
Levi Lais <levilais@gmail.com>
2018-03-05 14:02:10 -0800

    created HighScore objects

---
+++ Cats And Dogs/CustomObjects/HighScoreObject.swift
@@ -0,0 +1,25 @@
+//
+//  HighScoreObject.swift
+//  Cats And Dogs
+//
+//  Created by Levi on 3/5/18.
+//  Copyright © 2018 App Volks. All rights reserved.
+//
+
+import Foundation
+
+class HighScore {
+    var score: Int?
+    var playerName: String?
+    var timestamp: Date?
+    
+    func formattedScore(score: Int) -> String? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        if let formattedNumber = numberFormatter.string(from: score as! NSNumber) {
+            return formattedNumber
+        } else {
+            return nil
+        }
+    }
+}
