a7c91fe259bb166b9843490b98a720f8147365a0
Levi Lais <levilais@gmail.com>
2018-04-08 10:04:28 -0700

    Achievement buttons and labels are now in collections. Time to start setting them.

--- Cats And Dogs/AchievementsViewController.swift
+++ Cats And Dogs/AchievementsViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class AchievementsViewController: UIViewController {
 
     @IBOutlet weak var backgroundBottomLayout: NSLayoutConstraint!
+    @IBOutlet weak var bronzeToggle: UIButton!
+    @IBOutlet weak var silverToggle: UIButton!
+    @IBOutlet weak var goldToggle: UIButton!
+    
+    @IBOutlet var achievementButtons: [UIButton]!
+    @IBOutlet var achievementLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +28,23 @@ class AchievementsViewController: UIViewController {
         backgroundBottomLayout.constant -= (self.view.safeAreaInsets.bottom)
 
     }
-
+    
+    @IBAction func achievementButtonDidPress(_ sender: Any) {
+    }
+    
+    
+    @IBAction func bronzeToggleDidPress(_ sender: Any) {
+        print("bronze toggle pressed")
+    }
+    
+    @IBAction func silverToggleDidPress(_ sender: Any) {
+        print("silver toggle pressed")
+    }
+    
+    @IBAction func goldToggleDidPress(_ sender: Any) {
+        print("gold toggle pressed")
+    }
+    
     @IBAction func xButtonDidPress(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
