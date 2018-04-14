1d27593c8067c7f16b352972353c9eff3f34bd3e
Levi Lais <levilais@gmail.com>
2018-04-14 11:40:31 -0700

    fixed achievement verbiage

--- Cats And Dogs/Utilities/AchievementDataHelper.swift
+++ Cats And Dogs/Utilities/AchievementDataHelper.swift
@@ -112,55 +112,55 @@ class AchievementDataHelper {
     // REAL DATA
     static let achievements = [
         "pointGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to score",
-            "detailDescriptionEnd": "points.",
+            "detailDescriptionBeginning": "To earn this achievement, score",
+            "detailDescriptionEnd": "points in a single game.",
             "bronzeGoal": 7000000.0,
             "silverGoal": 10000000.0,
             "goldGoal": 25000000.0,
             "textTag": "Points"
         ],
         "missesGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to let",
-            "detailDescriptionEnd": "drops hit the ground.",
+            "detailDescriptionBeginning": "To earn this achievement, let",
+            "detailDescriptionEnd": "drops hit the ground in a single game.",
             "bronzeGoal": 500.0,
             "silverGoal": 750.0,
             "goldGoal": 1500.0,
             "textTag": "Misses"
         ],
         "zeroToOneHundredGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need see your miss meter drop to",
-            "detailDescriptionEnd": "and then fill it all the way back to 100",
+            "detailDescriptionBeginning": "To earn this achievement, your miss meter needs to drop to",
+            "detailDescriptionEnd": "and then fill all the way back to 100 in a single game.",
             "bronzeGoal": 60.0,
             "silverGoal": 30.0,
             "goldGoal": 10.0,
             "textTag": "To 100"
         ],
         "skipsGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to let",
-            "detailDescriptionEnd": "level up drops hit the ground.",
+            "detailDescriptionBeginning": "To earn this achievement, let",
+            "detailDescriptionEnd": "level up drops hit the ground in a single game.",
             "bronzeGoal": 10.0,
             "silverGoal": 15.0,
             "goldGoal": 30.0,
             "textTag": "Skips"
         ],
         "inARowGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to get",
-            "detailDescriptionEnd": "combos in a row.",
+            "detailDescriptionBeginning": "To earn this achievement, get",
+            "detailDescriptionEnd": "combos in a row in a single game.",
             "bronzeGoal": 75.0,
             "silverGoal": 125.0,
             "goldGoal": 200.0,
             "textTag": "In-A-Row"
         ],
         "millionInMinutesGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to get",
-            "detailDescriptionEnd": "points in 10 minutes.",
+            "detailDescriptionBeginning": "To earn this achievement, get",
+            "detailDescriptionEnd": "points in a single game in under 10 minutes.",
             "bronzeGoal": 5000000.0,
             "silverGoal": 7000000.0,
             "goldGoal": 10000000.0,
             "textTag": "In 10M"
         ],
         "dropGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to pop a single drop worth",
+            "detailDescriptionBeginning": "To earn this achievement, pop a single drop worth",
             "detailDescriptionEnd": "points.",
             "bronzeGoal": 30000.0,
             "silverGoal": 50000.0,
@@ -168,7 +168,7 @@ class AchievementDataHelper {
             "textTag": "Drop"
         ],
         "minutesOfTimeGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to stay alive for",
+            "detailDescriptionBeginning": "To earn this achievement, stay alive for",
             "detailDescriptionEnd": "minutes.",
             "bronzeGoal": 600.0,
             "silverGoal": 900.0,
@@ -176,16 +176,16 @@ class AchievementDataHelper {
             "textTag": "Minutes"
         ],
         "noComboGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to earn",
-            "detailDescriptionEnd": "points without getting a single combo.",
-            "bronzeGoal": 1000.0,
-            "silverGoal": 5000.0,
-            "goldGoal": 7500.0,
+            "detailDescriptionBeginning": "To earn this achievement, earn",
+            "detailDescriptionEnd": "points in a single game without getting a single combo.",
+            "bronzeGoal": 10000.0,
+            "silverGoal": 15000.0,
+            "goldGoal": 20000.0,
             "textTag": "No Combo"
         ],
         "hitsGoal": [
-            "detailDescriptionBeginning": "To earn this achievement, you will need to pop",
-            "detailDescriptionEnd": "drops.",
+            "detailDescriptionBeginning": "To earn this achievement, pop",
+            "detailDescriptionEnd": "drops in a single game.",
             "bronzeGoal": 1000.0,
             "silverGoal": 1500.0,
             "goldGoal": 2500.0,
